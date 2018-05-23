935481a20e3c421d765dbbe52c9b723f27edc275
Mitsuo Utano <utano320@gmail.com>
2018-05-23 14:03:35 +0900

    name save in localstorage

--- components/pages/admin/_room_id.js
+++ components/pages/admin/_room_id.js
@@ -8,7 +8,7 @@ export default {
   data() {
     return {
       roomId: this.$route.params.room_id,
-      name: 'teacher',
+      name: '',
       msg: '',
       socket: null,
       logs: [],
@@ -16,17 +16,20 @@ export default {
     };
   },
   mounted: function() {
-    this.socket = io(process.env.WS_HOST);
-    this.socket.on('message', this.addLogs);
-    this.socket.on('room', this.setMembers);
+    setTimeout(() => {
+      this.name = this.initName();
 
-    this.socket.json.emit('join', {
-      room: this.roomId,
-      name: this.name
-    });
+      this.socket = io(process.env.WS_HOST);
+      this.socket.on('message', this.addLogs);
+      this.socket.on('room', this.setMembers);
+
+      this.socket.json.emit('join', { room: this.roomId, name: this.name });
+    }, 200);
   },
   methods: {
     send() {
+      if (this.msg === '') return;
+
       this.socket.json.emit('send', {
         room: this.roomId,
         name: this.name,
@@ -37,13 +40,22 @@ export default {
     },
     addLogs(data) {
       Vue.set(this.logs, this.logs.length, data);
+      this.scrollLogs();
+    },
+    scrollLogs() {
       setTimeout(() => {
         let chatBoxList = document.getElementById('chatbox-list');
         chatBoxList.scrollTop = chatBoxList.scrollHeight - chatBoxList.clientHeight;
       }, 200);
     },
     setMembers(data) {
+      Vue.set(this.$data, 'logs', data.logs);
       Vue.set(this.$data, 'members', data.members);
+      this.scrollLogs();
+    },
+    initName() {
+      if (localStorage.getItem('name')) return localStorage.getItem('name');
+      return 'teacher';
     }
   }
 };
